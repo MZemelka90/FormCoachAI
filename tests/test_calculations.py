d77cf4e2fd98a170a745439c6ea156501d19e1bc
MarcZ <marc.zemelka@web.de>
2025-01-02 14:05:25 +0100

    Add unittests for the calculate_distance function

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -59,5 +59,38 @@ class TestCalculateTwoPointAngle(unittest.TestCase):
         self.assertAlmostEqual(calculate_two_point_angle(a, b), 0.0, places=5)
 
 
+class TestCalculateDistance(unittest.TestCase):
+
+    def test_horizontal_distance(self):
+        a = (0, 0)
+        b = (1, 0)
+        self.assertAlmostEqual(calculate_distance(a, b), 1.0, places=5)
+
+    def test_vertical_distance(self):
+        a = (0, 0)
+        b = (0, 1)
+        self.assertAlmostEqual(calculate_distance(a, b), 1.0, places=5)
+
+    def test_diagonal_distance(self):
+        a = (0, 0)
+        b = (1, 1)
+        self.assertAlmostEqual(calculate_distance(a, b), 1.4142135623730951, places=5)
+
+    def test_negative_coordinates(self):
+        a = (-1, 0)
+        b = (1, 0)
+        self.assertAlmostEqual(calculate_distance(a, b), 2.0, places=5)
+
+    def test_zero_distance(self):
+        a = (1, 1)
+        b = (1, 1)
+        self.assertAlmostEqual(calculate_distance(a, b), 0.0, places=5)
+
+    def test_all_negative_coordinates(self):
+        a = (-1, -1)
+        b = (-2, -2)
+        self.assertAlmostEqual(calculate_distance(a, b), 1.4142135623730951, places=5)
+
+
 if __name__ == '__main__':
     unittest.main()
